1bab11eba089da67cfdc281f915c2a4ae0978749
Nemesis <szakacsmpaula@gmail.com>
2022-02-01 09:34:12 +0100

    get changes

--- src/App.js
+++ src/App.js
@@ -1,8 +1,28 @@
-
-import './App.css';
+import "./App.css";
+// import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+// import Navbar from "./components/navbar.js";
 
 function App() {
- 
+//   return (
+//     <Router>
+//       <div className="App">
+//         <Navbar />
+        
+//           <Route exact path="/" />
+//           <Home />
+
+//           <Route path="/midiKeyboard" />
+//           <MidiKeyboard />
+
+//           <Route path="/drumKit" />
+//           <DrumKit />
+
+//           <Route path="/musicPlayer" />
+//           <MusicPlayer />
+       
+//       </div>
+//     </Router>
+//   );
 }
 
 export default App;
---
+++ src/components/navbar.js
@@ -0,0 +1,49 @@
+import { NavLink } from "react-router-dom";
+import "./styles/navbar.css";
+
+function Navbar() {
+  return (
+    <nav>
+      <ul className="nav">
+        <li>
+          <NavLink
+            // activeClassName="selected"
+            className="nav-link"
+            to="/"
+          >
+            <h1>PRP-Productions</h1>
+          </NavLink>
+        </li>
+        <li>
+          <NavLink
+            // activeClassName="selected"
+            className="nav-link"
+            to="/midiKeyboard"
+          >
+            MidiKeyboard
+          </NavLink>
+        </li>
+        <li>
+          <NavLink
+            // activeClassName="selected"
+            className="nav-link"
+            to="/drumKit"
+          >
+            DrumKit
+          </NavLink>
+        </li>
+        <li>
+          <NavLink
+            // activeClassName="selected"
+            className="nav-link"
+            to="/musicPlayer"
+          >
+            MusicPlayer
+          </NavLink>
+        </li>
+      </ul>
+    </nav>
+  );
+}
+
+export default Navbar;
