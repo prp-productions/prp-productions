ad53faca18561a7e617dcd310c03429b3b2796a3
Nemesis <szakacsmpaula@gmail.com>
2022-02-01 09:17:01 +0100

    get components ans styles

--- src/components/home.js
+++ src/components/home.js
@@ -1,19 +1,15 @@
 import { Link } from "react-router-dom";
 import "../styles/home.css";
 
-function Home(props) {
+function Home() {
   return (
-    <div
-      className="home-component"
-      style={{ backgroundImage: `url()` }}
-    >
+    <div className="home-component">
       <h2>
-        Welcome, we are <Link to="/about">PRP-Productions</Link>. We are
+        Welcome, this is <Link to="/midiKeyboard">PRP-Productions</Link>. We are
         building a platform on which users can learn about music by creating
         their own compositions. The app will support learning, writing,
         recording, and file sharing with other users.
       </h2>
-      <i className="fas fa-envelope" aria-hidden="true"></i> Hire us
     </div>
   );
 }
---
+++ src/pages/routes.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import { Route, IndexRoute } from 'react-router';
+
+/**
+ * Import all page components here
+ */
+import Home from './components/home.js';
+import DrumKit from '.components/drumKit.js';
+import MidiKeyboard from './components/midiKeyboard.js';
+import MusicPlayer from './components/musicPlayer.js';
