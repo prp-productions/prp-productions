88a92e7caf3492021e8438946741027b9cef094b
Nemesis <szakacsmpaula@gmail.com>
2022-02-02 09:37:26 +0100

    change colors

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import { Navbar } from "./components/Navbar.js";
 import { PageHome } from "./pages/PageHome";
 import { PageMidiKeyboard } from "./pages/PageMidiKeyboard";
-import { PageDrumKit } from "./pages/PageDrumKit";
+// import { PageDrumKit } from "./pages/PageDrumKit";
 import { PageMusicPlayer } from "./pages/PageMusicPlayer";
 
 function App() {
