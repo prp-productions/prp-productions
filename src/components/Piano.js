541938ab8ef84df1ae826daf66b4907b3af3ca1d
degi100 <r.degering@mail.de>
2022-02-03 14:54:37 +0100

    useRef works

--- src/components/Piano.js
+++ src/components/Piano.js
@@ -1,7 +1,16 @@
+import { useRef, useEffect } from 'react';
+
 export const Piano = () => {
+	const pianoElem = useRef(null);
+
+	useEffect(() => {
+		pianoElem.current.innerHTML = 'Hallo2'
+	})
+	
 	return(
 		<div>
 			<h1>Hallo</h1>
+			<div ref={pianoElem}></div>
 		</div>
 	)
 }
