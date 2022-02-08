60a2c5234607a0c0db4850d7b48bb9791b10dcd1
Nemesis <szakacsmpaula@gmail.com>
2022-02-08 14:05:35 +0100

    state variables in

--- src/components/MusicPlayer.js
+++ src/components/MusicPlayer.js
@@ -1,7 +1,15 @@
+import { useState } from "react";
+
 export const MusicPlayer = () => {
+    const[songs, setSongs] = useState(["hey", "prp", "beat01"]);
+    const[currentSongIndex, setCurrentSongIndex] = useState(0);
     return (
       <div>
-        <h1>Musicplayer </h1>
+        <h1>Musicplayer</h1>
       </div>
     );
-  };
+
+
+   
+}
+   
