140d48695a413547b06aee65c2633e4b9b5a844a
heavypete <70510263+heavypete@users.noreply.github.com>
2022-02-09 16:47:24 +0100

    worked on functions, not yet functional

--- src/classes/RecordingManager.js
+++ src/classes/RecordingManager.js
@@ -1,57 +1,83 @@
 import { AudioManager } from "./AudioManager.js";
+import { useState, useEffect, useRef } from "react";
 
 const audioManager = new AudioManager();
 
-export class RecordingManager {
-  constructor() {
-    this.isRecording = false;
-    this.waveForm = "sine";
-    this.recordingArray = [];
-    this.recordButtonElem = document.querySelector(".recordButton");
-    this.playButtonElem = document.querySelector(".playRecorded");
-    let changeColorRecordMode = 0;
+export const RecordingManager = () => {
+  const [isRecording, setIsRecording] = useState(false);
+  const [waveform, setWaveform] = useState("sine");
+  const [recordingArray, setRecordingArray] = useState([]);
 
-    this.recordButtonElem.addEventListener("click", () => {
-      const colors = ["red", "white"];
-      this.recordButtonElem.style.backgroundColor =
-        colors[changeColorRecordMode];
-      this.recordButtonElem.style.color = "black";
-      changeColorRecordMode =
-        changeColorRecordMode >= colors.length - 1
-          ? 0
-          : changeColorRecordMode + 1;
+  //? this.recordButtonElem = document.querySelector(".recordButton");
 
-      this.isRecording = !this.isRecording;
+  const [recordingIsPlaying, setRecordingIsPlaying] = useState(false);
+
+  let changeColorRecordMode = 0;
+
+  useEffect(() => {
+    recordButtonElem.current.addEventListener("on", () => {
+      setIsRecording(true);
     });
+  }, []);
 
-    this.playButtonElem.addEventListener("click", () => {
-      console.log(this.isRecording);
-      if (this.isRecording) {
-        console.log("ERROR");
-      } else {
-        console.log("recordingArray", this.recordingArray);
-        this.recordingArray.forEach((midiNote, index) => {
-          setTimeout(() => {
-            audioManager.noteOn(midiNote.note, midiNote.velocity);
-            audioManager.noteOffWithDuration(
-              midiNote.note,
-              midiNote.playDuration
-            ); // laenge der note
-          }, (midiNote.playDuration + midiNote.waitDuration) * index); // abstand vom anfang der ersten Note bis zur naechsten Note
-        });
-      }
-    });
-  }
+  const handleRecord = () => {
+    setIsRecording(!isRecording);
+  };
 
-  recordIfNecessary(midiNote) {
-    if (this.isRecording) this.recordingArray.push(midiNote);
-    console.log(this.recordingArray);
-  }
-
-  overwriteDurationOfLastNoteIfIsNecessary(duration) {
-    if (this.recordingArray.length > 0) {
-      this.recordingArray[this.recordingArray.length - 1].waitDuration =
-        duration;
+  const recordIfNecessary = (midiNote) => {
+    if (isRecording) {
+      setRecordingArray(recordingArray.push(midiNote));
     }
-  }
-}
+    console.log(recordingArray);
+  };
+
+  const playRecording = () => {
+    if ((isRecording = true)) {
+      console.log("ERROR, I'm recording now!");
+    } else {
+      recordingArray.forEach((midiNote, index) => {
+        setTimeout(() => {
+          audioManager.noteOn(midiNote.note, midiNote.velocity);
+          audioManager.noteOffWithDuration(
+            midiNote.note,
+            midiNote.playDuration
+          );
+        }, (midiNote.playDuration + midiNote.waitDuration) * index);
+      });
+    }
+  };
+
+  // this.playButtonElem.addEventListener("click", () => {
+  //     console.log(this.isRecording);
+  //     if (this.isRecording) {
+  //       console.log("ERROR");
+  //     } else {
+  //       console.log("recordingArray", this.recordingArray);
+  //       this.recordingArray.forEach((midiNote, index) => {
+  //         setTimeout(() => {
+  //           audioManager.noteOn(midiNote.note, midiNote.velocity);
+  //           audioManager.noteOffWithDuration(
+  //             midiNote.note,
+  //             midiNote.playDuration
+  //           ); // laenge der note
+  //         }, (midiNote.playDuration + midiNote.waitDuration) * index); // abstand vom anfang der ersten Note bis zur naechsten Note
+  //       });
+  //     }
+  //   });
+  // }
+  const overwriteDurationOfLastNoteIfIsNecessary = () => {
+    if (recordingArray.length > 0) {
+      recordingArray[recordingArray.length - 1].waitDuration = duration;
+    }
+  };
+  const recordButtonElem = useRef(null);
+
+  return (
+    <div className="recording-manager">
+      <div className="button-container">
+        <button className="record-button"></button> //* also stops recording
+        <button className="play-stop-button"></button>
+      </div>
+    </div>
+  );
+};
