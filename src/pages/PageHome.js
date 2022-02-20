6c4d34c78a00ee3b1915ca02eae1ddf798cc4fb8
heavypete <70510263+heavypete@users.noreply.github.com>
2022-02-20 15:14:48 +0100

    name drop and city colors

--- src/pages/PageHome.js
+++ src/pages/PageHome.js
@@ -1,15 +1,25 @@
 import { Link } from "react-router-dom";
-import "../styles/home.css";
+import "../styles/homepage.scss";
 
 export const PageHome = () => {
   return (
     <div className="home-component">
-    <h1>PRP-Productions</h1>
-      <h2>
-        Welcome, this is <Link to="">PRP-Productions</Link>. We
-        are building a platform on which users can learn about music by creating
-        their own compositions. The app will support learning, writing,
-        recording, and file sharing with other users.
+      <h1>
+        <div className="swing-top-fwd">PRP-Productions</div>
+      </h1>
+      <div className="tagline">
+        <div class="tagline-container">
+          <span class="title-word title-word-1">Software built in </span>
+          <span class="title-word title-word-2">Berlin, </span>
+          <span class="title-word title-word-3">Bremen, </span>
+          <span class="title-word title-word-4">Hamburg</span>
+        </div>
+      </div>
+      <h2 className="description">
+        Welcome, this is PRP-Productions. We are building a platform on which
+        users can learn about music by creating their own compositions. The app
+        will support learning, writing, recording, and file sharing with other
+        users.
       </h2>
     </div>
   );
